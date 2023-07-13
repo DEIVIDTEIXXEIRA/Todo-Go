04c6e14afa80ad903d98ebcf89a4518e20b6ed9a
DEIVIDTEIXXEIRA <deividteixeira.go@gmail.com>
2023-07-13 15:34:24 -0300

    variaveis de ambiente configuradas, conexão com o banco sendo aberta

--- API/src/router/rotas/usuarios.go
+++ API/src/router/rotas/usuarios.go
@@ -1,39 +1,39 @@
 package rotas
 
 import (
-	"api/src/router/controllers"
+	"api/src/controllers"
 	"net/http"
 )
 
 var rotasDeUsuarios = []Rota{
 	{
-		Uri:                "/usuarios",
-		Metodo:             http.MethodPost,
-		Funcao:            controllers.CriarUsuario,
+		Uri:                   "/usuarios",
+		Metodo:                http.MethodPost,
+		Funcao:                controllers.CriarUsuario,
 		RequerAutententicacao: false,
 	},
 	{
-		Uri:                "/usuarios",
-		Metodo:             http.MethodGet,
-		Funcao:            controllers.BuscarUsuarios,
+		Uri:                   "/usuarios",
+		Metodo:                http.MethodGet,
+		Funcao:                controllers.BuscarUsuarios,
 		RequerAutententicacao: false,
 	},
 	{
-		Uri:                "/usuarios/{usuarioId}",
-		Metodo:             http.MethodGet,
-		Funcao:            controllers.BuscarUsuario,
+		Uri:                   "/usuarios/{usuarioId}",
+		Metodo:                http.MethodGet,
+		Funcao:                controllers.BuscarUsuario,
 		RequerAutententicacao: false,
 	},
 	{
-		Uri:                "/usuarios/{usuarioId}",
-		Metodo:             http.MethodPut,
-		Funcao:            controllers.EditarUsuario,
+		Uri:                   "/usuarios/{usuarioId}",
+		Metodo:                http.MethodPut,
+		Funcao:                controllers.EditarUsuario,
 		RequerAutententicacao: false,
 	},
 	{
-		Uri:                "/usuarios/{usuarioId}",
-		Metodo:             http.MethodDelete,
-		Funcao:            controllers.DeletarUsuario,
+		Uri:                   "/usuarios/{usuarioId}",
+		Metodo:                http.MethodDelete,
+		Funcao:                controllers.DeletarUsuario,
 		RequerAutententicacao: false,
 	},
 }
